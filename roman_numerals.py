eae8631acbb5b970fca9f007c18614089d14f0de
ravenjoswell <rjc.panlaqui@gmail.com>
2024-06-08 10:49:21 -1000

    completed algo

--- roman_numerals.py
+++ roman_numerals.py
@@ -1,3 +1,30 @@
 def to_roman(num):
-    # write your code here!
-    pass
+    output = ""
+    roman_numeral_arabic = {
+        "M" : 1000,
+        "CM": 900,
+        "D" : 500, 
+        "CD": 400,
+        "C" : 100, 
+        "L" : 50, 
+        "XL": 40,
+        "X" : 10, 
+        "IX": 9, 
+        "V" : 5, 
+        "IV": 4,
+        "I" : 1, 
+    }
+    # goal is get a key , value out of a dict
+    for roman, arabic in roman_numeral_arabic.items():
+        temp = num // arabic
+        if temp == 0:
+            continue
+        
+        num -= temp * arabic
+        output += roman * temp
+       
+    return output 
+
+
+print(to_roman(944))
+ 
